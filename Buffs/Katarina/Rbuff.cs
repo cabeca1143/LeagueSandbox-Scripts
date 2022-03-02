95a75ce1ede4d8209e9b87d4ef48e60b0d8e0cf6
Cosmin <gusto_cs@yahoo.com>
2022-03-02 04:04:29 +0200

    Updated Katarina Buffs
    Updated Katarina's Buffs . 
    R Damage and calculations are now done in the BuffScript rather than SpellScript

---
+++ Buffs/Katarina/Rbuff.cs
@@ -0,0 +1,98 @@
+﻿using System.Numerics;
+using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Enums;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using GameServerCore.Scripting.CSharp;
+using System.Linq;
+using GameServerCore;
+
+
+namespace Buffs
+{
+    class KatarinaR : IBuffGameScript
+    {
+        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        {
+            BuffType = BuffType.COMBAT_DEHANCER,
+            BuffAddType = BuffAddType.RENEW_EXISTING,
+            MaxStacks = 1
+        };
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+
+        private IObjAiBase Owner;
+        float somerandomTick;
+        IAttackableUnit Target1;
+        IAttackableUnit Target2;
+        IAttackableUnit Target3;
+        float finaldamage;
+        IParticle p;
+
+        ISpell spell;
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            IChampion champion = unit as IChampion;
+            Owner = ownerSpell.CastInfo.Owner;
+            spell = ownerSpell;
+            var owner = spell.CastInfo.Owner;
+            var AP = spell.CastInfo.Owner.Stats.AbilityPower.Total * 0.25f;
+            var AD = spell.CastInfo.Owner.Stats.AttackDamage.FlatBonus * 0.375f;
+            float damage = 15f + ( 20f * spell.CastInfo.SpellLevel) + AP + AD;
+            finaldamage = damage;
+            p = AddParticleTarget(owner, owner, "Katarina_deathLotus_cas.troy", owner, lifetime: 2.5f, bone: "C_BUFFBONE_GLB_CHEST_LOC");
+
+
+            var champs = GetChampionsInRange(owner.Position, 500f, true).OrderBy(enemy => Vector2.Distance(enemy.Position, owner.Position)).ToList();
+            if (champs.Count > 3)
+            {
+                foreach (var enemy in champs.GetRange(0, 4)
+                     .Where(x => x.Team == CustomConvert.GetEnemyTeam(owner.Team)))
+                {
+                    SpellCast(owner, 0, SpellSlotType.ExtraSlots, true, enemy, owner.Position);
+                    if (Target1 == null) Target1 = enemy;
+                    else if (Target2 == null) Target2 = enemy;
+                    else if (Target3 == null) Target3 = enemy;
+                    enemy.TakeDamage(Owner, finaldamage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
+                }
+            }
+            else
+            {
+                foreach (var enemy in champs.GetRange(0, champs.Count)
+                    .Where(x => x.Team == CustomConvert.GetEnemyTeam(owner.Team)))
+                {
+                    SpellCast(owner, 0, SpellSlotType.ExtraSlots, true, enemy, owner.Position);
+                    if (Target1 == null) Target1 = enemy;
+                    else if (Target2 == null) Target2 = enemy;
+                    else if (Target3 == null) Target3 = enemy;
+                    enemy.TakeDamage(Owner, finaldamage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
+                }
+            }
+        }
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            RemoveParticle(p);
+            PlayAnimation(Owner, "Idle", 1);
+        }
+
+        public void OnPreAttack(ISpell spell)
+        {
+        }
+
+        public void OnUpdate(float diff)
+        {
+            somerandomTick += diff;
+            if (somerandomTick >= 250f)
+            {
+                if (Target1 != null) Target1.TakeDamage(Owner, finaldamage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
+                if (Target2 != null) Target2.TakeDamage(Owner, finaldamage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
+                if (Target3 != null) Target3.TakeDamage(Owner, finaldamage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
+                somerandomTick = 0;
+            }
+
+        }
+    }
+}
